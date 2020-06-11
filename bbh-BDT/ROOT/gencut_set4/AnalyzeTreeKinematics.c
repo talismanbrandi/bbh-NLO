e98016075e860fb9ba441d71d7b9cd6b0c66ecf2
Ayan Paul <apaul2@alumni.nd.edu>
2020-06-11 16:58:14 +0200

    modified BDT

--- bbh-BDT/ROOT/gencut_set4/AnalyzeTreeKinematics.c
+++ bbh-BDT/ROOT/gencut_set4/AnalyzeTreeKinematics.c
@@ -7,13 +7,16 @@ void AnalyzeTreeKinematics()
 {
 // Change lines 9, 10, 15 and 34. class = 0 for background and 1 for signal
     TFile file("bdt_bbxaa.root");
-    TTree* tree = (TTree*) file.Get("bbxaa");
+    TTree* tree = (TTree*) file.Get("yb2");
+	Float_t nbjet, njjet;
     Float_t ptb1, ptb2, pta1, pta2, ptaa;
     Float_t etab1, etab2, etaa1, etaa2, etaaa;
     Float_t mbb, maa, mb1h, mbbh;
     Float_t drbamin, drba1, dphiba1, dphibb;
     Float_t weight;
-    int type = 0;
+    int type = 4;
+    tree->SetBranchAddress("nbjet", &nbjet);
+    tree->SetBranchAddress("njjet", &njjet);
     tree->SetBranchAddress("ptb1", &ptb1);
     tree->SetBranchAddress("ptb2", &ptb2);
     tree->SetBranchAddress("pta1", &pta1);
@@ -34,10 +37,11 @@ void AnalyzeTreeKinematics()
     tree->SetBranchAddress("dphibb", &dphibb);
     tree->SetBranchAddress("weight", &weight);
     std::ofstream myfile;
-    myfile.open("bbxaa.csv");
-    myfile << "ptb1,ptb2,pta1,pta2,ptaa,etab1,etab2,etaa1,etaa2,etaaa,mbb,maa,mb1h,mbbh,drbamin,drba1,dphiba1,dphibb,class,weight" << std::endl;
+    myfile.open("yb2.csv");
+    myfile << "nbjet,njjet,ptb1,ptb2,pta1,pta2,ptaa,etab1,etab2,etaa1,etaa2,etaaa,mbb,maa,mb1h,mbbh,drbamin,drba1,dphiba1,dphibb,class,weight" << std::endl;
     for (int i = 0, N = tree->GetEntries(); i < N; ++i) {
         tree->GetEntry(i);
+		myfile << nbjet << "," << njjet << ",";
         myfile << ptb1 << "," << ptb2 << "," << pta1 << "," << pta2 << "," << ptaa << ",";
         myfile << etab1 << "," << etab2 << "," << etaa1 << "," << etaa2 << "," << etaaa << ",";
         myfile << mbb << "," << maa << "," << mb1h << "," << mbbh << ",";
