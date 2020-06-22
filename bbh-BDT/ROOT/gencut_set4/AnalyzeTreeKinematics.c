c519c623a478fc02f173af6baad0fe831f87ba99
Ayan Paul <apaul2@alumni.nd.edu>
2020-06-22 19:58:51 +0200

    work for fcc and hl-lhc. extreme bdt gains

--- bbh-BDT/ROOT/gencut_set4/AnalyzeTreeKinematics.c
+++ bbh-BDT/ROOT/gencut_set4/AnalyzeTreeKinematics.c
@@ -6,15 +6,16 @@
 void AnalyzeTreeKinematics()
 {
 // Change lines 9, 10, 15 and 34. class = 0 for background and 1 for signal
-    TFile file("bdt_bbxaa.root");
-    TTree* tree = (TTree*) file.Get("yb2");
+    TFile file("bdt_bbxaa2.root");
+    TTree* tree = (TTree*) file.Get("bbxaa");
 	Float_t nbjet, njjet;
     Float_t ptb1, ptb2, pta1, pta2, ptaa;
     Float_t etab1, etab2, etaa1, etaa2, etaaa;
     Float_t mbb, maa, mb1h, mbbh;
+    Float_t met, ht;
     Float_t drbamin, drba1, dphiba1, dphibb;
     Float_t weight;
-    int type = 4;
+    int type = 0;
     tree->SetBranchAddress("nbjet", &nbjet);
     tree->SetBranchAddress("njjet", &njjet);
     tree->SetBranchAddress("ptb1", &ptb1);
@@ -31,20 +32,23 @@ void AnalyzeTreeKinematics()
     tree->SetBranchAddress("maa", &maa);
     tree->SetBranchAddress("mb1h", &mb1h);
     tree->SetBranchAddress("mbbh", &mbbh);
+    tree->SetBranchAddress("met", &met);
+    tree->SetBranchAddress("ht", &ht);
     tree->SetBranchAddress("drbamin", &drbamin);
     tree->SetBranchAddress("drba1", &drba1);
     tree->SetBranchAddress("dphiba1", &dphiba1);
     tree->SetBranchAddress("dphibb", &dphibb);
     tree->SetBranchAddress("weight", &weight);
     std::ofstream myfile;
-    myfile.open("yb2.csv");
-    myfile << "nbjet,njjet,ptb1,ptb2,pta1,pta2,ptaa,etab1,etab2,etaa1,etaa2,etaaa,mbb,maa,mb1h,mbbh,drbamin,drba1,dphiba1,dphibb,class,weight" << std::endl;
+    myfile.open("bbxaa_2.csv");
+    myfile << "nbjet,njjet,ptb1,ptb2,pta1,pta2,ptaa,etab1,etab2,etaa1,etaa2,etaaa,mbb,maa,mb1h,mbbh,met,ht,drbamin,drba1,dphiba1,dphibb,class,weight" << std::endl;
     for (int i = 0, N = tree->GetEntries(); i < N; ++i) {
         tree->GetEntry(i);
 		myfile << nbjet << "," << njjet << ",";
         myfile << ptb1 << "," << ptb2 << "," << pta1 << "," << pta2 << "," << ptaa << ",";
         myfile << etab1 << "," << etab2 << "," << etaa1 << "," << etaa2 << "," << etaaa << ",";
         myfile << mbb << "," << maa << "," << mb1h << "," << mbbh << ",";
+        myfile << met << "," << ht << ",";
         myfile << drbamin << "," << drba1 << "," << dphiba1 << "," << dphibb << "," << type << "," << weight << std::endl;
     }
     myfile.close();
